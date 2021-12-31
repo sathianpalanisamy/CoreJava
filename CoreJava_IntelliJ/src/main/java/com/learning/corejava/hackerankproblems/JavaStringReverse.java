ea1c510bda3bf9cba3a6b10f3d29ba9de65c44ed
sathian123 <38859861+sathian123@users.noreply.github.com>
2021-12-31 14:39:37 +0530

    removing unwanted code

--- CoreJava_IntelliJ/src/main/java/com/learning/corejava/hackerankproblems/JavaStringReverse.java
+++ CoreJava_IntelliJ/src/main/java/com/learning/corejava/hackerankproblems/JavaStringReverse.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class JavaStringReverse {
     public static void main(String[] args) {
-//        Scanner scn = new Scanner(System.in);
-//        String word = scn.next();
-//        scn.close();
-//
-//        String reverse ="";
-//        for (int i=word.length()-1;i>=0;i--){
-//            reverse+=word.charAt(i);
-//        }
-//        System.out.println(reverse.equals(word)?"Yes":"No");
+        Scanner scn = new Scanner(System.in);
+        String word = scn.next();
+        scn.close();
+
+        String reverse ="";
+        for (int i=word.length()-1;i>=0;i--){
+            reverse+=word.charAt(i);
+        }
+        System.out.println(reverse.equals(word)?"Yes":"No");
 
     }
 }
--- CoreJava_IntelliJ/src/main/java/com/learning/corejava/hackerankproblems/ValidUserNameRegularExpression.java
+++ CoreJava_IntelliJ/src/main/java/com/learning/corejava/hackerankproblems/ValidUserNameRegularExpression.java
@@ -1,55 +1,11 @@
 package com.learning.corejava.hackerankproblems;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidUserNameRegularExpression {
     public static void main(String[] args) {
-////        String regex = "[a-zA-Z][\\w_]{7,29}";
-////        System.out.println(Pattern.compile(regex).matcher("1sathiana").matches());
-//
-//        String line = "aa.(aaeee),aaa.(s@;;;[#))))$_-a)";
-
-        String line = "aa.(s@;;(;[#`)[ $_-a)";
-
-//        Pattern r = Pattern.compile("([^1]+).(\\([^1]+\\))");
-
-//        Pattern r = Pattern.compile("([\\w\\W]*).\\(([\\w\\W]+)\\)");
-
-//        Pattern r = Pattern.compile("([\\w\\W]+).(\\([\\w\\W]+\\))");
-
-//        Pattern r = Pattern.compile("\\[([\\w\\W]*)\\].\\[([\\w\\W]+)\\]");
-
-//        Pattern r = Pattern.compile("\\[([\\w\\W]+)\\].\\[([\\w@;#//(//)_\\-$\\[\\]]+)\\],");
-
-
-//        Pattern r = Pattern.compile("([\\w\\s\\d]*).(\\([\\w\\s\\d]+\\))");
-
-
-        Pattern r = Pattern.compile("([\\w\\s\"\\-\\[\\]/.^@;$*#+_?{}|—/]*).(\\([\\w\\s\"\\-\\[\\]\\(/.^@;$`()*#+_?{}|—/]+\\))");
-
-
-//        Pattern r = Pattern.compile("([\\w\\s\"\\-\\[\\]/.^@;$()*+_?{}|—/]*).(\\([\\w\"/.^$*[//]+-?{}|—/]+\\))");
-
-//
-//        Pattern r = Pattern.compile("([\\w\"/.^$*\\[//\\]+-?{}|—/]*).(\\([\\w\"/.^$*\\[//\\]+-?{}|—/]+\\))");
-////        Pattern r = Pattern.compile("([^.]*).(\\([\\w/.^$*[//]+-?{}|—/]+\\))");
-////        Pattern r = Pattern.compile("<(.+)\\)([^<]+)</\\1\\)");
-        Matcher m = r.matcher(line);
-        System.out.println(m.groupCount());
-        while (m.find()) {
-            System.out.println(m.group(1)+"                      "+m.group(2));
-        }
-
-
-        Map<String,String> db = new HashMap<>();
-//        db.put("s@;;;[#()=( $_-a","a");
-
-        db.put("a","a");
-
-        System.out.println(db);
+        String regex = "[a-zA-Z][\\w_]{7,29}";
+        System.out.println(Pattern.compile(regex).matcher("1sathiana").matches());
 
 
     }
