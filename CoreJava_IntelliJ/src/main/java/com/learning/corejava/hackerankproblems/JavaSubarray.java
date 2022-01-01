0b4b1f6d6ba31e22db852a978aa54cc412e83012
sathian123 <38859861+sathian123@users.noreply.github.com>
2022-01-01 12:36:05 +0530

    Create JavaSubarray.java

---
+++ CoreJava_IntelliJ/src/main/java/com/learning/corejava/hackerankproblems/JavaSubarray.java
@@ -0,0 +1,49 @@
+package com.learning.corejava.hackerankproblems;
+
+import java.util.Scanner;
+
+public class JavaSubarray {
+    public static void main(String[] args) {
+
+        try (Scanner scn = new Scanner(System.in)) {
+
+            int size = scn.nextInt();
+            scn.nextLine();
+            String lines = scn.nextLine();
+            String[] line = lines.split(" ");
+            int[] array = new int[size];
+
+            for (int i = 0; i < line.length; i++) {
+                array[i] = Integer.parseInt(line[i]);
+            }
+//            int count = 0;
+//
+//            for (int i = 0; i < array.length; i++) {
+//                for (int j = i; j < array.length; j++) {
+//                    int sum = 0;
+//                    for (int k = i; k <= j; k++) {
+//                        sum += array[k];
+//                    }
+//                    if (sum < 0) {
+//                        count++;
+//                    }
+//
+//                }
+//            }
+
+            int count=0;
+            for(int j=0;j< array.length;j++){
+                int sum=0;
+                for(int k=j;k< array.length;k++){
+                    sum=array[k]+sum;
+                    if(sum<0){
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+
+
+        }
+    }
+}
